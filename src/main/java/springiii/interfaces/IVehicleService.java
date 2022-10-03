12014bc2043a3114cbacaa86d96ddc3f9f6f8398
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-10-03 13:34:05 -0300

    feat: interface VEhicleService created

--- src/main/java/springiii/interfaces/IVehicleService.java
+++ src/main/java/springiii/interfaces/IVehicleService.java
@@ -1,5 +1,14 @@
 package springiii.interfaces;
 
-public interface IVehicleService {
+import springiii.entity.Vehicle;
 
+import java.util.Date;
+import java.util.List;
+
+public interface IVehicleService {
+    List<Vehicle> getAll();
+    Vehicle get(String board);
+    List<Vehicle> getForPrice(double since, double to);
+    List<Vehicle> getForDate(Date since, Date to);
+    Vehicle create(Vehicle vehicle);
 }
